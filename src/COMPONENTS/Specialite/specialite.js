c28c038afd9da8ffd8aa6df69363ba75228a607c
RaouaneDev <raouanedev@gmail.com>
2022-09-28 17:28:56 +0200

    change colors

--- src/COMPONENTS/Specialite/specialite.js
+++ src/COMPONENTS/Specialite/specialite.js
@@ -3,23 +3,23 @@ import React from 'react'
 
 const Specialite = () => {
   return (
-    <div className='specialite bg-white p-5 '>
-        <div className='container bg-warning p-5'>
+    <div className='specialite bg-warning p-5 '>
+        <div className='container bg-white p-5 rounded'>
             <div className='grid'>
-                <div className='row'>
+                <div className='row '>
                     <div className='col-md-4  '>
-                        <h2 >Bilan</h2>
-                        <p className='  lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue, une bonne tenue de la comptabilité et une gestion optimisée sont les garanties de réussite et de pérennité pour une entreprise.</p>
+                        <h2 className=' ' >Bilan</h2>
+                        <p className='  lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue .</p>
                     
                     </div>
                     <div className='col-md-4 bg-dark text-white p-3'>
                         <h2>Bilan</h2>
-                        <p className='lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue, une bonne tenue de la comptabilité et une gestion optimisée sont les garanties de réussite et de pérennité pour une entreprise.</p>
+                        <p className='lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue .</p>
                     
                     </div>
                     <div className='col-md-4'>
                         <h2>Bilan</h2>
-                        <p className='lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue, une bonne tenue de la comptabilité et une gestion optimisée sont les garanties de réussite et de pérennité pour une entreprise.</p>
+                        <p className='lead align-items-center'>Il est connu de tous que les experts comptables sont indispensables pour une entreprise. Mis à part l’évolution du contexte économique dans lequel elle évolue.</p>
                     
                     </div>
                 </div>
